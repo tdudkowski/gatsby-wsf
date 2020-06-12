574cb21fbe9c4bbb12be286e26fa491f73143926
tdudkowski <tdudkowski@gmail.com>
2020-06-12 03:57:25 +0200

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,5 +42,16 @@ module.exports = {
         display: "swap",
       },
     },
+    {
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        // your google analytics tracking id
+        trackingId: `UA-2105423-14`,
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // enable ip anonymization
+        anonymize: true,
+      },
+    },
   ],
 }
